09e09a0dcb5888fca383dad3c4fa203b292914eb
stasinos <konstant@iit.demokritos.gr>
2011-05-09 03:11:19 +0000

    First fuzzy trains dataset, from Jan 2008
    git-svn-id: https://yadlr.svn.sourceforge.net/svnroot/yadlr/trunk@4 a9cf1dd1-0f30-4ebf-82f6-f38f5c8fc227

--- examples/trains/domain.pl
+++ examples/trains/domain.pl
@@ -1,70 +1,39 @@
 
-%%
-%% declarations
-%%
+%:- consult( '../aleph_dlsettings.pl' ).
+
+:- declare_concept( train ),
+	declare_concept( car ),
+	declare_concept( load ),
+	declare_concept( load_state ),
+	declare_concept( station ).
 
 
-:- declare_concept( 'Train' ),
-	declare_concept( 'Car' ),
-	declare_concept( 'Load' ),
-	declare_concept( 'Wheel' ),
-	declare_concept( 'Open' ),
-	declare_concept( 'Long' ),
-	declare_concept( 'Rectangle' ),
-	declare_concept( 'Triangle' ),
-	declare_concept( 'Hexagon' ),
-	declare_concept( 'Circle' ).
-
-:- declare_relation( hasCar, 'Train', 'Car' ),
-	declare_relation( hasLoad, 'Car', 'Load' ),
-	declare_relation( hasWheel,'Car', 'Wheel' ),
-	declare_relation( inFront, 'Car', 'Car' ).
+:- add_to_concept( train,      [(t1,0.88), (t2,0.9), (t3,0.8), (t4,1.0)] ).
+:- add_to_concept( car,        [(c11,0.9), (c21,0.95), (c22,0.8), (c31,0.8), (c41,0.9), (c42,1.0)] ).
+:- add_to_concept( load,       [(l311,1.0), (l312,1.0), (l411,1.0), (l412,1.0), (l421,0.9), (l422,0.8)] ).
+:- add_to_concept( load_state, [(gas,1.0),(liquid,1.0),(solid,1.0)] ).
+:- add_to_concept( station,    [(st1,1.0),(st2,1.0)] ).
 
 
-%%
-%% east1 (eastbound)
-%%
+:-      declare_relation( has_car,   train, car),
+	declare_relation( has_load,  car,   load),
+	declare_relation( has_state, load,  load_state).
 
-:- add_to_concept( 'Train', [(east1,0.98)] ).
+:-      add_to_relation( has_car, t1, [(c11,0.9)]).
 
-:- add_to_concept( 'Car',   [(car11,1.0), (car12,1.0), (car13,1.0), (car14,1.0)] ).
-:- add_to_concept( 'Open',     [(car11,0.9), (car12,0.1), (car13,0.8), (car14,0.9)] ).
-:- add_to_concept( 'Long',     [(car11,0.9), (car12,0.1), (car13,0.8), (car14,0.1)] ).
-:- add_to_concept( 'Rectangle',[(car11,0.9), (car12,0.9), (car13,0.8), (car14,0.9)] ).
-:- add_to_concept( 'Triangle', [(car11,0.1), (car12,0.1), (car13,0.05),(car14,0.1)] ).
-:- add_to_concept( 'Hexagon',  [(car11,0.02),(car12,0.1), (car13,0.2), (car14,0.1)] ).
-:- add_to_concept( 'Circle',   [(car11,0.01),(car12,0.1), (car13,0.11),(car14,0.1)] ).
+:-      add_to_relation( has_car, t2, [(c21,0.3),(c22,0.9)] ).
 
-:- add_to_concept( 'Load',  [(load111,1.0), (load112,0.9), (load113,0.9),
-			     (load121,1.0), (load131,1.0), (load141,1.0) ] ).
-:- add_to_concept( 'Rectangle',[(load111,1.0), (load112,0.9), (load113,0.9),
-				(load121,0.1), (load131,0.1), (load141,0.05) ] ).
-:- add_to_concept( 'Triangle', [(load111,0.1), (load112,0.2), (load113,0.1),
-				(load121,1.0), (load131,0.1), (load141,0.1) ] ).
-:- add_to_concept( 'Hexagon',  [(load111,0.1), (load112,0.1), (load113,0.05),
-				(load121,0.1), (load131,1.0), (load141,0.01) ] ).
-:- add_to_concept( 'Circle',   [(load111,0.1), (load112,0.09),(load113,0.09),
-				(load121,0.1), (load131,0.1), (load141,1.0) ] ).
+:-      add_to_relation( has_car,  t3,  [(c31,0.9)] ),
+	add_to_relation( has_load, c31, [(l311,1.0),(l312,1.0)] ).
 
-:- add_to_concept( 'Wheel', [(wheel111,1.0), (wheel112,1.0),
-			     (wheel121,1.0), (wheel122,1.0),
-			     (wheel131,1.0), (wheel132,1.0), (wheel133,1.0),
-			     (wheel141,1.0), (wheel142,1.0)] ).
+:-      add_to_relation( has_car,  t4,  [(c41,0.9),(c42,0.88)] ),
+	add_to_relation( has_load, c41, [(l411,1.0),(l412,1.0)] ),
+	add_to_relation( has_load, c42, [(l421,1.0),(l422,1.0)] ).
 
-:- add_to_relation( hasCar, east1, [(car11,1.0)] ),
-	add_to_relation( inFront, car11, [(car12,1.0)] ),
-	add_to_relation( inFront, car12, [(car13,1.0)] ),
-	add_to_relation( inFront, car13, [(car14,1.0)] ).
-
-:- add_to_relation( hasLoad,  car11, [ (load111,1.0),  (load112,1.0), (load113,1.0)] ).
-:- add_to_relation( hasWheel, car11, [(wheel111,1.0), (wheel112,1.0)] ).
-		    
-:- add_to_relation( hasLoad,  car12, [(load121,1.0)] ).
-:- add_to_relation( hasWheel, car12, [(wheel121,1.0), (wheel122,1.0)] ).
-
-:- add_to_relation( hasLoad,  car13, [(load131,1.0)] ).
-:- add_to_relation( hasWheel, car13, [(wheel131,1.0), (wheel132,1.0), (wheel133,1.0)] ).
-
-:- add_to_relation( hasLoad,  car14, [(load141,1.0)] ).
-:- add_to_relation( hasWheel, car14, [(wheel141,1.0), (wheel142,1.0)] ).
+:-      add_to_relation( has_state, l411, [(gas,0.55)] ),
+	add_to_relation( has_state, l412, [(solid,0.8)] ),
+	add_to_relation( has_state, l311, [(solid,0.8)] ),
+	add_to_relation( has_state, l312, [(liquid,0.9)] ),
+	add_to_relation( has_state, l421, [(liquid,0.87)] ),
+	add_to_relation( has_state, l422, [(gas,0.5)] ).
 
---
+++ examples/trains/trains.b
@@ -0,0 +1,2 @@
+:- consult( '../aleph_dlsettings.pl' ).
+:- consult( 'trains1_domain' ).
